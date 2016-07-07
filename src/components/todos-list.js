9006379d39d8fd10c07e198b750e1614afae7e5f
Theo Carnavos <tcarnavos@gmail.com>
2016-07-07 02:26:46 -0500

    create todo-list-item component with item name and edit/delete buttons

--- src/components/todos-list.js
+++ src/components/todos-list.js
@@ -1,18 +1,31 @@
+import _ from 'lodash';
 import React from 'react';
 import TodosListHeader from './todos-list-header';
+import TodosListItem from './todos-list-item';
+
+// todos-list contains renderItems function which maps todos
+// (located in App main component (hardcoded for now) and passes into tbody inside render)
 
 export default class TodosList extends React.Component {
   renderItems() {
-    return 
+    // lodash map
+    return _.map(this.props.todos, (todo, index) => <TodosListItem key={index
+      // es6 spreading, same as task=(todo.task) isCompleted={todo.isCompleted}
+      } {...todo} />);
+
+    // same as
+    // function(todo, index) {
+    //   return <TodosListItem />
+    // }
   }
   render() {
     console.log(this.props.todos);
     return (
       <table>
         <TodosListHeader />
-        <tr>
+        <tbody>
           {this.renderItems()}
-        </tr>
+        </tbody>
       </table>
     );
   }
